73dd2f7f5cf678c51dd30b6fb6677f2a8334cbb6
Derek Allen Hearst <derekallenhearst@gmail.com>
2018-11-10 11:26:36 -0800

    here

--- PacMAN/packman.js
+++ PacMAN/packman.js
@@ -2,9 +2,30 @@ function setup() {
   pacX = 0;
   pacY = 0;
 
-  createCanvas(2000, 2000);
+  createCanvas(700, 700);
 }
 
+//This function draws the actual pac man sprite
 function draw() {
-  el
+  fill(255);
+  rect(0,0,690,690);
+  fill(255,255,0);
+  nostroke();
+  ellipse(pacX, pacY, 50, 50);
+}
+
+function keyPressed() {
+  var keyIndex = -1;
+  if(key == 'a' && pacX >=50) {
+    pacX--;
+  }
+  if(key == 'w' && pacY >=50){
+    pacY++;
+  }
+  if(key == 'd' && packX<=950){
+    pacX++;
+  }
+  if(key == 's' && packY<=950){
+    packY--;
+  }
 }
