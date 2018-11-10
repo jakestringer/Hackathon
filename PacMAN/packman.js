ecaa73f3005661c82f8132ad5dc8ae58246dd86e
Derek Allen Hearst <derekallenhearst@gmail.com>
2018-11-10 11:05:46 -0800

    Update PacMAN/packman.js

--- PacMAN/packman.js
+++ PacMAN/packman.js
@@ -1,12 +1,10 @@
 function setup() {
-  createCanvas(640, 480);
+  pacX = 0;
+  pacY = 0;
+
+  createCanvas(2000, 2000);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+  el
 }
